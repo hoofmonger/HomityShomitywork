5da5b19c2932ed8a262d151aafe32cf5e0b54068
James Hoofard <james.hoofard.iii@gmail.com>
2021-08-05 18:03:32 -0500

    fixed some bugs

--- Password Generator/Develop/script.js
+++ Password Generator/Develop/script.js
@@ -11,25 +11,25 @@ numericalCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let characterLength = parseInt(prompt("How many characters do you want your password to be? Please choose between 8 and 128. "));
 console.log(characterLength);
 
-if (characterLength === Number)
 
 
-lowerCasePrompt = confirm("Do you want lower case letters?")
+
+let lowerCasePrompt = confirm("Do you want lower case letters?")
 ;
 console.log(lowerCasePrompt);
 
-upperCasePrompt = confirm("Do you want upper case letters?")
+let upperCasePrompt = confirm("Do you want upper case letters?")
 
 console.log(upperCasePrompt);
 
-specialCharacterPrompt = confirm("Do you want special characters?")
+let specialCharacterPrompt = confirm("Do you want special characters?")
 
-sole.log(specialCharacterPrompt);
+console.log(specialCharacterPrompt);
 
 numericalCharacterPrompt = confirm("Do you want numerical Characters")
 console.log(numericalCharacterPrompt);
 chosenValues = [...lowerCase, ...upperCase, ...specialCharacter, ...numericalCharacter]
-
+if (characterLength === Number)
 if (lowerCasePrompt, numericalCharacterPrompt, specialCharacterPrompt, upperCasePrompt)
 if (numericalCharacterPrompt, specialCharacterPrompt, upperCasePrompt)
 if (specialCharacterPrompt, upperCasePrompt)
@@ -39,26 +39,16 @@ if (lowerCasePrompt, upperCasePrompt)
 if (lowerCasePrompt, upperCasePrompt, specialCharacterPrompt)
 if (lowerCasePrompt, upperCasePrompt, numericalCharacterPrompt)
 if (lowerCasePrompt, specialCharacterPrompt, numericalCharacterPrompt)
-function getRandomCharacter(chosenValues){
-  var character = arr[Math.floor(Math.random()*chosenValues.length)]
-  console.log(character)
-
-
+chosenValues.push.getRandomCharacter(chosenValues);
   
-return chosenValues.push
-
-}
-
-
+  console.log(chosenValues)
 }
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  var passwordText = document.querySelector(password);
+  passwordText.value = finalPassword;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
