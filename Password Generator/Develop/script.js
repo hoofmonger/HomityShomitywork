8f3257a0166151bf6b52f058865934cc2ad462e6
James Hoofard <james.hoofard.iii@gmail.com>
2021-08-05 17:50:12 -0500

    added lots of if statements to js

--- Password Generator/Develop/script.js
+++ Password Generator/Develop/script.js
@@ -7,43 +7,49 @@ lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 specialCharacter = [" ", "!", "#", "@", "$", "%", "&", "'", ")", "(", "*", "+", ",", "-", ".", "/", ";", ":", "<", ">", "?", "[", "]", "{", "}", "|", "~", "`"];
 numericalCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-chosenValues = [lowerCase, upperCase, specialCharacter, numericalCharacter]
+
 let characterLength = parseInt(prompt("How many characters do you want your password to be? Please choose between 8 and 128. "));
 console.log(characterLength);
 
 if (characterLength === Number)
 
 
-let lowerCasePrompt = confirm("Do you want lower case letters?")
-if (lowerCasePrompt === true);
+lowerCasePrompt = confirm("Do you want lower case letters?")
+;
 console.log(lowerCasePrompt);
 
-let upperCasePrompt = confirm("Do you want upper case letters?")
-if (upperCasePrompt === true);
+upperCasePrompt = confirm("Do you want upper case letters?")
+
 console.log(upperCasePrompt);
 
-let specialCharacterPrompt = confirm("Do you want special characters?")
-if (specialCharacterPrompt === true);
+specialCharacterPrompt = confirm("Do you want special characters?")
+
 sole.log(specialCharacterPrompt);
 
-let numericalCharacterPrompt = confirm("Do you want numerical Characters")
-if (numericalCharacterPrompt = true)
+numericalCharacterPrompt = confirm("Do you want numerical Characters")
 console.log(numericalCharacterPrompt);
-chosenValues.push(arr)
+chosenValues = [...lowerCase, ...upperCase, ...specialCharacter, ...numericalCharacter]
 
-
-
+if (lowerCasePrompt, numericalCharacterPrompt, specialCharacterPrompt, upperCasePrompt)
+if (numericalCharacterPrompt, specialCharacterPrompt, upperCasePrompt)
+if (specialCharacterPrompt, upperCasePrompt)
+if (upperCasePrompt)
+if (lowerCasePrompt)
+if (lowerCasePrompt, upperCasePrompt)
+if (lowerCasePrompt, upperCasePrompt, specialCharacterPrompt)
+if (lowerCasePrompt, upperCasePrompt, numericalCharacterPrompt)
+if (lowerCasePrompt, specialCharacterPrompt, numericalCharacterPrompt)
+function getRandomCharacter(chosenValues){
+  var character = arr[Math.floor(Math.random()*chosenValues.length)]
+  console.log(character)
 
 
   
-return getRandomCharacter.chosenValues
+return chosenValues.push
 
 }
-if (isNaN(parsed)) {return 8;}
-     return parsed *128;
-function getRandomCharacter(arr){
-  var character = arr[Math.floor(Math.random()*arr.length)]
-  console.log(character)
+
+
 }
 // Write password to the #password input
 function writePassword() {
